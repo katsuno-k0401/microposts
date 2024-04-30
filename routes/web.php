3902c8710d42803ec76528acf7866d263807efbd
kosuke.katsuno <tech24-1@78it.com>
2024-04-30 02:21:23 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 //use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsersController; // 追記
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController;  // 追記
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,19 +18,24 @@ use App\Http\Controllers\UsersController; // 追記
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::get('/', [MicropostsController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        // 追記ここから
+    Route::prefix('users/{id}')->group(function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');
+    });
+    // 追記きこまで
+    Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
 
 require __DIR__.'/auth.php';
